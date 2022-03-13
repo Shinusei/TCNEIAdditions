4bb7ec6e1dd6e38eeb79a7185c77d2ebe7de8394
miozune <miozune@gmail.com>
2022-03-13 18:18:20 +0900

    Use IMC

--- src/main/java/ru/timeconqueror/tcneiadditions/nei/IMCForNEI.java
+++ src/main/java/ru/timeconqueror/tcneiadditions/nei/IMCForNEI.java
@@ -8,17 +8,22 @@ public class IMCForNEI {
         setNBTAndSend("ru.timeconqueror.tcneiadditions.nei.arcaneworkbench.ArcaneCraftingShapelessHandler", "Thaumcraft:blockTable:15");
         setNBTAndSend("ru.timeconqueror.tcneiadditions.nei.TCNACrucibleRecipeHandler", "Thaumcraft:blockMetalDevice");
         setNBTAndSend("ru.timeconqueror.tcneiadditions.nei.TCNAInfusionRecipeHandler", "Thaumcraft:blockStoneDevice:2");
+        setNBTAndSend("ru.timeconqueror.tcneiadditions.nei.AspectCombinationHandler", "Thaumcraft:ItemResearchNotes", 20, 16);
     }
 
-    private static void setNBTAndSend(String name, String stack) {
+    private static void setNBTAndSend(String name, String stack, int height, int maxRecipesPerPage) {
         NBTTagCompound NBT = new NBTTagCompound();
         NBT.setString("handler", name);
         NBT.setString("modName", "Thaumcraft");
         NBT.setString("modId", "Thaumcraft");
         NBT.setBoolean("modRequired", true);
         NBT.setString("itemName", stack);
-        NBT.setInteger("handlerHeight", 140);
-        NBT.setInteger("maxRecipesPerPage", 2);
+        NBT.setInteger("handlerHeight", height);
+        NBT.setInteger("maxRecipesPerPage", maxRecipesPerPage);
         FMLInterModComms.sendMessage("NotEnoughItems", "registerHandlerInfo", NBT);
+    }
+
+    private static void setNBTAndSend(String name, String stack) {
+        setNBTAndSend(name, stack, 140, 2);
     }
 }
--- src/main/java/ru/timeconqueror/tcneiadditions/nei/NEIConfig.java
+++ src/main/java/ru/timeconqueror/tcneiadditions/nei/NEIConfig.java
@@ -2,19 +2,15 @@ package ru.timeconqueror.tcneiadditions.nei;
 
 import codechicken.nei.api.API;
 import codechicken.nei.api.IConfigureNEI;
-import codechicken.nei.event.NEIRegisterHandlerInfosEvent;
 import com.djgiannuzz.thaumcraftneiplugin.nei.recipehandler.ArcaneShapedRecipeHandler;
 import com.djgiannuzz.thaumcraftneiplugin.nei.recipehandler.ArcaneShapelessRecipeHandler;
 import com.djgiannuzz.thaumcraftneiplugin.nei.recipehandler.AspectRecipeHandler;
 import com.djgiannuzz.thaumcraftneiplugin.nei.recipehandler.CrucibleRecipeHandler;
 import com.djgiannuzz.thaumcraftneiplugin.nei.recipehandler.InfusionRecipeHandler;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.item.ItemStack;
 import ru.timeconqueror.tcneiadditions.HandlerRemover;
 import ru.timeconqueror.tcneiadditions.TCNEIAdditions;
 import ru.timeconqueror.tcneiadditions.nei.arcaneworkbench.ArcaneCraftingShapedHandler;
 import ru.timeconqueror.tcneiadditions.nei.arcaneworkbench.ArcaneCraftingShapelessHandler;
-import thaumcraft.common.config.ConfigItems;
 
 public class NEIConfig implements IConfigureNEI {
 
@@ -44,14 +40,6 @@ public class NEIConfig implements IConfigureNEI {
         API.registerUsageHandler(new ArcaneCraftingShapelessHandler());
         API.registerUsageHandler(new TCNACrucibleRecipeHandler());
         API.registerUsageHandler(new TCNAInfusionRecipeHandler());
-    }
-
-    @SubscribeEvent
-    public void regHandlers(NEIRegisterHandlerInfosEvent event) {
-        event.registerHandlerInfo(AspectCombinationHandler.class, TCNEIAdditions.NAME, TCNEIAdditions.MODID, builder -> {
-            builder.setHeight(20);
-            builder.setDisplayStack(new ItemStack(ConfigItems.itemResearchNotes));
-        });
     }
 
     @Override
