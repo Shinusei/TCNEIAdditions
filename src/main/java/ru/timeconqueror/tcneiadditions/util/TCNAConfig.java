d1085daae1c033c66d94d3d6ad0b26c8b5ae3d37
miozune <miozune@gmail.com>
2022-10-17 22:18:44 +0900

    Make config translatable

--- src/main/java/ru/timeconqueror/tcneiadditions/util/TCNAConfig.java
+++ src/main/java/ru/timeconqueror/tcneiadditions/util/TCNAConfig.java
@@ -2,11 +2,16 @@ package ru.timeconqueror.tcneiadditions.util;
 
 import java.io.File;
 import net.minecraftforge.common.config.Configuration;
+import ru.timeconqueror.tcneiadditions.TCNEIAdditions;
 
 public class TCNAConfig {
     public static Configuration config;
 
-    public static final String GENERAL = "General";
+    public static final String GENERAL = "general";
+
+    public static final String[] CATEGORIES = new String[] {GENERAL};
+
+    private static final String LANG_PREFIX = TCNEIAdditions.MODID + ".config.";
 
     public static boolean showLockedRecipes;
     public static boolean showInstabilityNumber;
@@ -19,14 +24,20 @@ public class TCNAConfig {
 
     public static void syncConfig() {
         config.setCategoryComment(GENERAL, "General config");
+        config.setCategoryLanguageKey(GENERAL, LANG_PREFIX + GENERAL);
 
         showLockedRecipes = config.get(
                         GENERAL, "showLockedRecipes", false, "Show recipes even if the research is not completed")
+                .setLanguageKey(LANG_PREFIX + GENERAL + ".showLockedRecipes")
                 .getBoolean();
+
         showInstabilityNumber = config.get(
                         GENERAL, "showInstabilityNumber", true, "Show the number of instability of infusion")
+                .setLanguageKey(LANG_PREFIX + GENERAL + ".showInstabilityNumber")
                 .getBoolean();
+
         showResearchKey = config.get(GENERAL, "showResearchKey", true, "Show research key")
+                .setLanguageKey(LANG_PREFIX + GENERAL + ".showResearchKey")
                 .getBoolean();
 
         if (config.hasChanged()) {
